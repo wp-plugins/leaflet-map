ee376c2778485f07f2b24c0257e5d735200059d5
bozdoz <bozdoz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-28 21:34:28 +0000

    adding version 1.9
    git-svn-id: https://plugins.svn.wordpress.org/leaflet-map/tags/1.9@1034745 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- leaflet-map.php
+++ leaflet-map.php
@@ -3,8 +3,8 @@
     Plugin Name: Leaflet Map
     Plugin URI: http://twitter.com/bozdoz/
     Description: A plugin for creating a Leaflet JS map with a shortcode.
-    Version: 1.4
-    Author: Benjamin J DeLong
+    Version: 1.9
+    Author: bozdoz
     Author URI: http://twitter.com/bozdoz/
     License: GPL2
     */
@@ -18,6 +18,8 @@ if (!class_exists('Leaflet_Map_Plugin')) {
                 'leaflet_map_tile_url' => 'http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg',
                 'leaflet_map_tile_url_subdomains' => '1234',
                 'leaflet_js_version' => '0.7.3',
+                'leaflet_js_url' => 'http://cdn.leafletjs.com/leaflet-%s/leaflet.js',
+                'leaflet_css_url' => 'http://cdn.leafletjs.com/leaflet-%s/leaflet.css',
                 'leaflet_default_zoom' => '16',
                 'leaflet_default_height' => '250',
                 'leaflet_default_width' => '100%',
@@ -36,6 +38,8 @@ if (!class_exists('Leaflet_Map_Plugin')) {
                 'leaflet_map_tile_url' => 'See some example tile URLs at <a href="http://developer.mapquest.com/web/products/open/map" target="_blank">MapQuest</a>.  Can be set per map with shortcode attribute <br/> <code>[leaflet-map tileurl="http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg"]</code>',
                 'leaflet_map_tile_url_subdomains' => 'Some maps get tiles from multiple servers with subdomains such as a,b,c,d or 1,2,3,4; can be set per map with the shortcode <br/> <code>[leaflet-map subdomains="1234"]</code>',
                 'leaflet_js_version' => '0.7.3 is newest as of this plugin\'s conception',
+                'leaflet_js_url' => 'If you host your own Leaflet files, specify the URL here: add a single "%s" to the url to specify where the version number goes, if necessary.',
+                'leaflet_css_url' => 'Same as above.',
                 'leaflet_default_zoom' => 'Can set per map in shortcode or adjust for all maps here; e.g. <br /> <code>[leaflet-map zoom="5"]</code>',
                 'leaflet_default_height' => 'Can set per map in shortcode or adjust for all maps here. Values can include "px" but it is not necessary.  Can also be %; e.g. <br/> <code>[leaflet-map height="250"]</code>',
                 'leaflet_default_width' => 'Can set per map in shortcode or adjust for all maps here. Values can include "px" but it is not necessary.  Can also be %; e.g. <br/> <code>[leaflet-map height="250"]</code>',
@@ -44,25 +48,24 @@ if (!class_exists('Leaflet_Map_Plugin')) {
                 'leaflet_scroll_wheel_zoom' => 'Disable zoom with mouse scroll wheel.  Sometimes someone wants to scroll down the page, and not zoom the map.  Enabled or disable per map in shortcode: <br/> <code>[leaflet-map scrollwheel="0"]</code>',
             );
 
-        /* count map shortcodes to allow for multiple */
-        public static $leaflet_map_count;
-
-        /* leave marker variables global for possibly manipulation */
-        public static $leaflet_marker_count;
-
         public function __construct() {
             add_action('admin_init', array(&$this, 'admin_init'));
             add_action('admin_menu', array(&$this, 'admin_menu'));
+
             add_shortcode('leaflet-map', array(&$this, 'map_shortcode'));
             add_shortcode('leaflet-marker', array(&$this, 'marker_shortcode'));
+            add_shortcode('leaflet-image', array(&$this, 'image_shortcode'));
+
+            add_action( 'wp_enqueue_scripts', array(&$this, 'enqueue_and_register') );
+            add_action( 'admin_enqueue_scripts', array(&$this, 'enqueue_and_register') );
         }
 
         public static function activate () {
             /* set default values to db */
             foreach(self::$defaults as $arrs) {
-            	foreach($arrs as $k=>$v) {
-            		add_option($k, $v);
-            	}
+                foreach($arrs as $k=>$v) {
+                    add_option($k, $v);
+                }
             }
         }
         
@@ -79,8 +82,32 @@ if (!class_exists('Leaflet_Map_Plugin')) {
             foreach (self::$defaults as $arrs) {
                 foreach($arrs as $k=>$v) {
                     delete_option($k);
-            	}
+                }
             }
+        }
+
+        public function enqueue_and_register () {
+            $defaults = $this::$defaults['text'];
+
+            /* defaults from db */
+            $version = get_option('leaflet_js_version', $defaults['leaflet_js_version']);
+            $js_url = get_option('leaflet_js_url', $defaults['leaflet_js_url']);
+            $css_url = get_option('leaflet_css_url', $defaults['leaflet_css_url']);
+
+            $js_url = sprintf($js_url, $version);
+            $css_url = sprintf($css_url, $version);
+
+            // add style to every page
+            wp_enqueue_style('leaflet_stylesheet', $css_url, Array(), $version, false);
+
+            wp_register_script('leaflet_js', $js_url, Array(), $version, true);
+            
+            /* run an init function because other wordpress plugins don't play well with their window.onload functions */
+            wp_register_script('leaflet_map_init', plugins_url('scripts/init-leaflet-map.js', __FILE__), Array('leaflet_js'), '1.0', true);
+
+            /* run a construct function in the document head for the init function to use */
+            wp_enqueue_script('leaflet_map_construct', plugins_url('scripts/construct-leaflet-map.js', __FILE__), Array(), '1.0', false);
+
         }
         
         public function admin_init () {
@@ -101,9 +128,8 @@ if (!class_exists('Leaflet_Map_Plugin')) {
         }
 
         public function shortcode_page () {
-
             wp_enqueue_style( 'leaflet_admin_stylesheet' );
-            wp_enqueue_script('custom_plugin_js', plugins_url('scripts/get-shortcode.js', __FILE__), false);
+            wp_enqueue_script('custom_plugin_js', plugins_url('scripts/get-shortcode.js', __FILE__), Array('leaflet_js'), false);
 
             include 'templates/find-on-map.php';
         }
@@ -144,6 +170,9 @@ if (!class_exists('Leaflet_Map_Plugin')) {
             return (Object) array('lat' => 0, 'lng' => 0);
         }
 
+        /* count map shortcodes to allow for multiple */
+        public static $leaflet_map_count;
+
         public function map_shortcode ( $atts ) {
             
             if (!$this::$leaflet_map_count) {
@@ -164,11 +193,10 @@ if (!class_exists('Leaflet_Map_Plugin')) {
             $default_tileurl = get_option('leaflet_map_tile_url', $defaults['leaflet_map_tile_url']);
             $default_subdomains = get_option('leaflet_map_tile_url_subdomains', $defaults['leaflet_map_tile_url_subdomains']);
             $default_scrollwheel = get_option('leaflet_scroll_wheel_zoom', $defaults['leaflet_scroll_wheel_zoom']);
-            $version = get_option('leaflet_js_version', $defaults['leaflet_js_version']);
 
-            /* leaflet style and script */
-            wp_enqueue_style('leaflet_stylesheet', 'http://cdn.leafletjs.com/leaflet-'.$version.'/leaflet.css', false);
-            wp_enqueue_script('leaflet_js', 'http://cdn.leafletjs.com/leaflet-'.$version.'/leaflet.js', false);
+            /* leaflet script */
+            wp_enqueue_script('leaflet_js');
+            wp_enqueue_script('leaflet_map_init');
 
             if ($atts) {
                 extract($atts);
@@ -204,58 +232,130 @@ if (!class_exists('Leaflet_Map_Plugin')) {
             $content = '<div id="leaflet-wordpress-map-'.$leaflet_map_count.'" class="leaflet-wordpress-map" style="height:'.$height.'; width:'.$width.';"></div>';
 
             $content .= "<script>
-            var map_{$leaflet_map_count};
-            jQuery(function () {
-                var baseURL = '{$tileurl}';
-               
-                var base = L.tileLayer(baseURL, { 
-                   subdomains: '{$subdomains}'
-                   });
-                map_{$leaflet_map_count} = L.map('leaflet-wordpress-map-{$leaflet_map_count}', 
-                	{
-                		layers: [base],
-                		zoomControl: {$zoomcontrol},
-                		scrollWheelZoom: {$scrollwheel}
-                	}).setView([{$lat}, {$lng}], {$zoom});";
-			
-			if ($show_attr) {
-                /* add attribution to MapQuest and OSM */
-                $content .= 'map_'.$leaflet_map_count.'.attributionControl.addAttribution("Tiles Courtesy of <a href=\"http://www.mapquest.com/\" target=\"_blank\">MapQuest</a> <img src=\"http://developer.mapquest.com/content/osm/mq_logo.png\" />");';
-                $content .= 'map_'.$leaflet_map_count.'.attributionControl.addAttribution("© <a href=\"http://www.openstreetmap.org/\">OpenStreetMap</a> contributors");';
-            }
+            WPLeafletMapPlugin.add(function () {
+                var map,
+                    baseURL = '{$tileurl}',
+                    base = L.tileLayer(baseURL, { 
+                       subdomains: '{$subdomains}'
+                    });
+                
+                map = L.map('leaflet-wordpress-map-{$leaflet_map_count}', 
+                    {
+                        layers: [base],
+                        zoomControl: {$zoomcontrol},
+                        scrollWheelZoom: {$scrollwheel}
+                    }).setView([{$lat}, {$lng}], {$zoom});";
+                
+                if ($show_attr) {
+                    /* add attribution to MapQuest and OSM */
+                    $content .= '
+                        map.attributionControl.addAttribution("Tiles Courtesy of <a href=\"http://www.mapquest.com/\" target=\"_blank\">MapQuest</a> <img src=\"http://developer.mapquest.com/content/osm/mq_logo.png\" />");
 
-            $content .= '
-        	});
+                        map.attributionControl.addAttribution("© <a href=\"http://www.openstreetmap.org/\">OpenStreetMap</a> contributors");';
+                }
+
+                $content .= '
+                WPLeafletMapPlugin.maps.push(map);
+            }); // end add
             </script>
             ';
 
             return $content;
         }
 
-        public function marker_shortcode ( $atts ) {
+        public function image_shortcode ( $atts ) {
+            
+            /* get map count for unique id */
+            if (!$this::$leaflet_map_count) {
+                $this::$leaflet_map_count = 0;
+            }
+            $this::$leaflet_map_count++;
+
+            $leaflet_map_count = $this::$leaflet_map_count;
+
+            $defaults = array_merge($this::$defaults['text'], $this::$defaults['checks']);
+
+            /* defaults from db */
+            $default_zoom_control = get_option('leaflet_show_zoom_controls', $defaults['leaflet_show_zoom_controls']);
+            $default_height = get_option('leaflet_default_height', $defaults['leaflet_default_height']);
+            $default_width = get_option('leaflet_default_width', $defaults['leaflet_default_width']);
+            $default_scrollwheel = get_option('leaflet_scroll_wheel_zoom', $defaults['leaflet_scroll_wheel_zoom']);
+
+            /* leaflet script */
+            wp_enqueue_script('leaflet_js');
+            wp_enqueue_script('leaflet_map_init');
+
+            if ($atts) {
+                extract($atts);
+            }
+
+            /* only required field for image map */
+            $source = empty($source) ? 'http://lorempixel.com/1000/1000/' : $source;
+
+            /* check more user defined $atts against defaults */
+            $height = empty($height) ? $default_height : $height;
+            $width = empty($width) ? $default_width : $width;
+            $zoomcontrol = empty($zoomcontrol) ? $default_zoom_control : $zoomcontrol;
+            $zoom = empty($zoom) ? 1 : $zoom;
+            $scrollwheel = empty($scrollwheel) ? $default_scrollwheel : $scrollwheel;
+            
+            /* allow percent, but add px for ints */
+            $height .= is_numeric($height) ? 'px' : '';
+            $width .= is_numeric($width) ? 'px' : '';   
+            
+            $content = '<div id="leaflet-wordpress-image-'.$leaflet_map_count.'" class="leaflet-wordpress-map" style="height:'.$height.'; width:'.$width.';"></div>';
+
+            $content .= "<script>
+            WPLeafletMapPlugin.add(function () {
+                var map,
+                    image_src = '$source',
+                    img = new Image(),
+                    zoom = $zoom;
+
+                img.onload = function() {
+                    var center_h = img.height / (zoom * 4),
+                        center_w = img.width / (zoom * 4);
+
+                    map.setView([center_h, center_w], zoom);
+
+                    L.imageOverlay( image_src, [[ center_h * 2, 0 ], [ 0, center_w * 2]] ).addTo( map );
+
+                    img.is_loaded = true;
+                };
+                img.src = image_src;
+
+                map = L.map('leaflet-wordpress-image-{$leaflet_map_count}', {
+                    maxZoom: 10,
+                    minZoom: 1,
+                    crs: L.CRS.Simple,
+                    zoomControl: {$zoomcontrol},
+                    scrollWheelZoom: {$scrollwheel}
+                }).setView([0, 0], zoom);
+
+                // make it known that it is an image map
+                map.is_image_map = true;
+
+                WPLeafletMapPlugin.maps.push( map );
+                WPLeafletMapPlugin.images.push( img );
+            }); // end add
+            </script>";
+
+            return $content;
+        }
+
+        public function marker_shortcode ( $atts, $content = null ) {
 
             /* add to previous map */
             if (!$this::$leaflet_map_count) {
             	return '';
             }
 
-            /* increment marker count */
-            if (!$this::$leaflet_marker_count) {
-            	$this::$leaflet_marker_count = 0;
-            }
-            $this::$leaflet_marker_count++;
-
             $leaflet_map_count = $this::$leaflet_map_count;
-            $leaflet_marker_count = $this::$leaflet_marker_count;
-
-            $content = "<script>
-            var marker_{$leaflet_marker_count};
-            jQuery(function () {";
-
+            
             if (!empty($atts)) extract($atts);
 
             $draggable = empty($draggable) ? 'false' : $draggable;
-            $visible = ($visible == 'true');
+            $visible = empty($visible) ? false : ($visible == 'true');
 
             if (!empty($address)) {
                 $location = $this::google_geocode($address);
@@ -263,46 +363,82 @@ if (!class_exists('Leaflet_Map_Plugin')) {
                 $lng = $location->{'lng'};
             }
 
-            if (empty($lat) && empty($lng)) {
-            	/* add to previous map's center */
-            	$content .= "
-	            marker_{$leaflet_marker_count} = L.marker(map_{$leaflet_map_count}.getCenter()";
-            } else {
+            $marker_script = "<script>
+            WPLeafletMapPlugin.add(function () {
+                var marker,
+                    map_count = {$leaflet_map_count},
+                    draggable = {$draggable},
+                    previous_map = WPLeafletMapPlugin.maps[ map_count - 1 ],
+                    is_image = previous_map.is_image_map,
+                    image_len = WPLeafletMapPlugin.images.length,
+                    previous_image = WPLeafletMapPlugin.images[ image_len - 1 ],
+                    previous_image_onload;
+                ";
+
             	/* add to user contributed lat lng */
-	            $lat = empty($lat) ? '44.67' : $lat;
-	            $lng = empty($lng) ? '-63.61' : $lng;
-	            $content .= "
-	            marker_{$leaflet_marker_count} = L.marker([{$lat}, {$lng}]";
+                $lat = empty($lat) ? ( empty($y) ? '0' : $y ) : $lat;
+                $lng = empty($lng) ? ( empty($x) ? '0' : $x ) : $lng;
+
+	            $marker_script .= "
+	            marker = L.marker([{$lat}, {$lng}], { draggable : draggable });";
+
+
+                if (empty($lat) && empty($lng)) {
+                    /* update lat lng to previous map's center */
+                    $marker_script .= "
+                    if ( is_image && 
+                        !previous_image.is_loaded) {
+                        previous_image_onload = previous_image.onload;
+                        previous_image.onload = function () {
+                            previous_image_onload();
+                            marker.setLatLng( previous_map.getCenter() );
+                        };
+                    } else {
+                        marker.setLatLng( previous_map.getCenter() );
+                    }
+                    ";
+                }
+
+            $marker_script .= "
+            if (draggable) {
+                marker.on('dragend', function () {
+                    var latlng = this.getLatLng(),
+                        lat = latlng.lat,
+                        lng = latlng.lng;
+                    if (is_image) {
+                        console.log('[leaflet-marker y=' + lat + ' x=' + lng + ']');
+                    } else {
+                        console.log('[leaflet-marker lat=' + lat + ' lng=' + lng + ']');
+                    }
+                });
             }
 
-            $content .= ", { draggable : {$draggable} });
+            marker.addTo( previous_map );
             ";
             
+            $message = empty($message) ? (empty($content) ? '' : $content) : $message;
 
-            $content .= "
-            marker_{$leaflet_marker_count}.addTo(map_{$leaflet_map_count});
-            ";
-            
             if (!empty($message)) {
 
-                $content .= "marker_{$leaflet_marker_count}.bindPopup('$message')";
+                $marker_script .= "marker.bindPopup('$message')";
 
                 if ($visible) {
 
-                    $content .= ".openPopup()";                    
+                    $marker_script .= ".openPopup()";                    
 
                 }
 
-                $content .= ";
+                $marker_script .= ";
                 ";
 
             }
 
-            $content .= "
-            });
+            $marker_script .= "
+                    WPLeafletMapPlugin.markers.push( marker );
+            }); // end add function
             </script>";
 
-            return $content;
+            return $marker_script;
         }
 
     } /* end class */
--- readme.txt
+++ readme.txt
@@ -6,13 +6,13 @@ Contributors: bozdoz
 Donate link: https://www.gittip.com/bozdoz/
 Tags: leaflet, map, javascript, mapquest
 Requires at least: 3.0.1
-Tested up to: 4.0
-Version: 1.4
-Stable tag: 1.4
+Tested up to: 4.0.1
+Version: 1.9
+Stable tag: 1.9
 License: GPLv2
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-A flexible plugin that adds two shortcodes: one for creating multiple leaflet maps, and one for adding multiple markers to those leaflet maps.
+A flexible plugin that adds basic shortcodes for creating multiple Leaflet maps and adding multiple markers to those maps.
 
 == Description ==
 
@@ -36,7 +36,9 @@ The zoom buttons can be large and annoying.  Enabled or disable per map in short
 
 `[leaflet-map zoomcontrol="0"]`
 
-Add a marker to any map by adding `[leaflet-marker]` after any `[leaflet-map]` shortcode.  You can adjust the lat/lng in the same way, as well as some other basic functionality (popup message, draggable, visible on load).
+Add a marker to any map by adding `[leaflet-marker]` after any `[leaflet-map]` shortcode.  You can adjust the lat/lng in the same way, as well as some other basic functionality (popup message, draggable, visible on load).  Also, if you want to add a link to a marker popup, use `[leaflet-marker]Message here: click here[/leaflet-marker]` and add a link like you normally would with the WordPress editor.
+
+Alternatively, you could use a plain image for visitors to zoom and pan around with `[leaflet-image source="path/to/image/file.jpg"]`.  See screenshots 3 - 5 for help setting that up.
 
 Check out the source code on [GitHub](https://github.com/bozdoz/wp-plugin-leaflet-map)!
 
@@ -51,7 +53,19 @@ Check out the source code on [GitHub](https://github.com/bozdoz/wp-plugin-leafle
 
 * Can I add a message to a marker?
 
-Yes: [leaflet-marker message="Hello there!" visible="true"], where visible designates if it is visible on page load. Otherwise it is only visible when clicked.
+Yes: `[leaflet-marker message="Hello there!" visible="true"]`, where visible designates if it is visible on page load. Otherwise it is only visible when clicked.
+
+* Can I use your plugin with a picture instead of a map?
+
+Yes: Use `[leaflet-image source="path/to/image/file.jpg"]`.  See screenshots 3 - 5 for help setting that up. 
+
+* Can I use my own self-hosted Leaflet files?
+
+Yes: It's been added to the dashboard options! 
+
+* How can I add a link to a marker?
+
+Use the marker format `[leaflet-marker]Click here![/leaflet-marker]` and add a hyperlink like you normally would with the WordPress editor.
 
 Shoot me a question [@bozdoz](http://www.twitter.com/bozdoz/).
 
@@ -59,8 +73,28 @@ Shoot me a question [@bozdoz](http://www.twitter.com/bozdoz/).
 
 1. Put the shortcode into the post.
 2. See the shortcode play out on the front end.
+3. For `[leaflet-image]` upload an image, and copy the URL from the right-hand side
+4. For `[leaflet-image]` paste that image URL into an attribute titled `source`: example: `source="http://lorempixel.com/1000/1000/"`.
+5. See the `[leaflet-image]` on the front end.
+6. If you use `[leaflet-marker draggable=true]`, then you can drag the marker where you want it, open a developers console, and see the specific shortcode to use.
+7. You can specify the URL of your leaflet files, if you don't want to use the CDN url.
 
 == Changelog ==
+
+= 1.9 =
+* Added ability to use hyperlinks in marker messages.
+
+= 1.8 =
+* Added ability to self-host leaflet files.
+
+= 1.7 =
+* Uploaded 1.6 to subversion incorrectly!
+
+= 1.6 =
+* Important fix to conflicts with other plugins!
+
+= 1.5 =
+* Some helpful js fixes for multiple window onload functions, and added the `leaflet-image` shortcode!
 
 = 1.4 =
 * Some fixes for Google geocoding, and switched cookies to db options.
@@ -78,6 +112,21 @@ Shoot me a question [@bozdoz](http://www.twitter.com/bozdoz/).
 * First Version. Basic map creation and marker creation.
 
 == Upgrade Notice ==
+
+= 1.9 =
+Added ability to use hyperlinks in marker messages.
+
+= 1.8 =
+Added ability to self-host leaflet files.
+
+= 1.7 =
+Fix to the poor upload of 1.6
+
+= 1.6 =
+Removed windows onload functions and created a construct and init js file for initiating the maps when Leaflet is ready (other plugins were overwriting windows.onload).
+
+= 1.5 =
+Improved stability for multiple plugins with windows onload functions.
 
 = 1.4 =
 Stable Google geocoding.
